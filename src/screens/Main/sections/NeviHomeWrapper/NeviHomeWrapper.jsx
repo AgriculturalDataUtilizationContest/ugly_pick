58ffa98c7f48330c9eb126484de7d5a33af4e221
juuhub <juhankim@handong.ac.kr>
2025-06-28 16:59:30 +0900

    fix : home 리디렉션

--- src/screens/Main/sections/NeviHomeWrapper/NeviHomeWrapper.jsx
+++ src/screens/Main/sections/NeviHomeWrapper/NeviHomeWrapper.jsx
@@ -24,33 +24,42 @@ export const NeviHomeWrapper = ({
     navigate("/search");
   };
 
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className={`nevi-home-wrapper ${className}`}>
       <div className="frame-33">
-        <div className="item-link" onClick={() => navigate("/")}>
+        {/* HOME 텍스트 */}
+        <div className="item-link" onClick={handleHomeClick} style={{ cursor: "pointer" }}>
           HOME
         </div>
 
+        {/* FORECAST */}
         <div className="text-wrapper-25" onClick={handleForecastClick} style={{ cursor: "pointer" }}>
           FORECAST
         </div>
 
+        {/* SEARCH */}
         <div className="text-wrapper-25" onClick={handleSearchClick} style={{ cursor: "pointer" }}>
           SEARCH
         </div>
       </div>
 
-      <Group
-        divClassName="group-5"
-        divClassName1="group-5"
-        divClassNameOverride="group-5"
-        frameClassName="group-4"
-        frameClassNameOverride="group-6"
-        icon="icon-3"
-        iconClassName="group-3"
-        override={groupIcon}
-      />
+      {/* 왼쪽 아이콘에도 클릭 이벤트 추가 */}
+      <div onClick={handleHomeClick} style={{ cursor: "pointer", display: "inline-block" }}>
+        <Group
+          divClassName="group-5"
+          divClassName1="group-5"
+          divClassNameOverride="group-5"
+          frameClassName="group-4"
+          frameClassNameOverride="group-6"
+          icon="icon-3"
+          iconClassName="group-3"
+          override={groupIcon}
+        />
+      </div>
     </div>
   );
 };
-
