d9bb0b2940656c858120f73c766276ddf4d64cc5
sungyu0309 <tjsrbyt@gmail.com>
2025-06-29 00:05:21 +0900

    feat : api

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
-export const getCropsByCategory = (category) => {
+export const getCropsByCategory = async (category) => {
   try {
-    const url = `${process.env.REACT_APP_BASEURL}/api/forecast?category=${category}`;
+    console.log(category);
+    const url = `${process.env.REACT_APP_BASE_URL}/api/forecast/${category}`;
+    console.log(url);
 
-    const response = axios.get(url);
-  } catch (error) {}
+    const response = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log("ERROR : get crops by category", error);
+    throw error;
+  }
 };
