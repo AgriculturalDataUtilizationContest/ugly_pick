a00f6f3df2d9335a9a327c305606f941825c418a
sungyu0309 <tjsrbyt@gmail.com>
2025-06-30 03:43:15 +0900

    feat : 서비스 점검 시간

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import Router from "./Router";
 import ThemeProvider from "./theme/ThemeProvider";
+import Maintenance from "./pages/Maintenance";
+
 function App() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  const isMaintenanceTime = hours === 15 && minutes >= 0 && minutes < 40;
+
+  if (isMaintenanceTime) {
+    return <Maintenance />;
+  }
+
   return (
     <ThemeProvider>
       <Router />
---
+++ src/pages/Maintenance.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Maintenance() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        <h1>🚧 점검 중입니다 🚧</h1>
+        <h2>오후 3시부터 3시 40분까지 서비스 점검이 진행됩니다.</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Maintenance;
